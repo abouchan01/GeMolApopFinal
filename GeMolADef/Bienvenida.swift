fd543a1405761547af690504289c91dffcf600fa
abouchan01 <abrahambouchan@hotmail.com>
2019-05-18 21:47:25 -0500

    unzip all

---
+++ GeMolADef/Bienvenida.swift
@@ -0,0 +1,23 @@
+//
+//  Bienvenida.swift
+//  GeMolApop
+//
+//  Created by BRA on 22/10/18.
+//  Copyright © 2018 BouCo. All rights reserved.
+//
+
+import UIKit
+
+class Bienvenida: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
---
+++ GeMolADef/Vista1.swift
@@ -0,0 +1,26 @@
+//
+//  Vista1.swift
+//  GeMolApop
+//
+//  Created by Bouchan on 19/10/18.
+//  Copyright © 2018 BouCo. All rights reserved.
+//
+
+import UIKit
+
+class Vista1: UIViewController {
+    
+    @IBAction func Boton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
